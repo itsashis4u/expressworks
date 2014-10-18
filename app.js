041c58bf8dd4a18382b7636c7a009bea3aec5d11
Ashish <itsashis4u@gmail.com>
2014-10-18 20:31:32 +0530

    Changes committed to app.js

--- app.js
+++ app.js
@@ -1,37 +1,15 @@
 var express = require('express');
-var http = require('http');
-var path = require('path');
-var mongoose = require('mongoose');
 var app = express();
+var mongoose = require('mongoose');
 
-app.set('port', process.env.port || 9000);
-app.set('views', __dirname + '/views');
+app.get('/', function(req, res) {
+    res.end('Hello World!')
+})
 
-// app.use(express.bodyParser());
-// app.use(express.methodOverride());
-// app.use(app.router);
-app.use(express.static(path.join(__dirname, '/')));
+var port = 9000;
 
-mongoose.connect('mongodb://localhost/company');
-var Schema = mongoose.Schema({
-	username : String,
-	password : String
-});
-
-var user = mongoose.model('emp', Schema);
- 
-app.post('/new', function(req, res){
-	new user({
-		username: req.body.username,
-		password  : req.body.password				
-	}).save(function(err, doc){
-		if(err) res.json(err);
-		else    res.send('Successfully inserted!');
-	});
-});
- 
- 
- 
-var server = http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+app.listen(process.argv[2] || port);
+if (process.argv[2] === undefined)
+    console.log("Listening at http://localhost:" + port);
+else
+    console.log("Listening at http://localhost:" + process.argv[2]);
