5378aaf83989d0006b8e4511fb1a8b96b84b4e7f
Ashish <itsashis4u@gmail.com>
2014-10-19 02:39:58 +0530

    Serving index.html static file

--- app.js
+++ app.js
@@ -4,8 +4,17 @@ require('./db.js');
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var path = require('path')
+// app.set('views', path.join(__dirname, '/'))
+
+app.use(express.static(path.join(__dirname, '/')));
 
 app.get('/', function(req, res) {
+    res.end('Hello World!')
+})
+
+
+app.get('/users', function(req, res) {
     res.end('Hello World!')
 })
 
