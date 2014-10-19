41b5c8b472c46b7ea28b6978bedc6f4dd069912b
Amulya Kumar Sahoo <amulya349@gmail.com>
2014-10-19 19:12:41 +0530

    Tuned the code, added comments Fully Working

--- app.js
+++ app.js
@@ -6,18 +6,19 @@ var bodyParser = require('body-parser')
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart();
 
+var formPage = "/";  //Link of page which contains the form
 
 // app.set('views', path.join(__dirname, '/'))
 
 app.use(express.static(path.join(__dirname, '/')));
 
-app.use('/', multipartMiddleware);
+app.use(formPage, multipartMiddleware);
 
 app.get('/', function(req, res) {
     res.end('Hello World!')
 })
 
-
+//used for data communication using GET method
 app.get('/users', function(req, res) {
     res.end('Hello World!')
 })
@@ -28,10 +29,7 @@ app.listen(port);
 
 console.log("Listening at http://localhost:" + port);
 
-
-
-
-
+//Connect to MongoDb
 mongoose.connect('mongodb://localhost/test');
 
 var db = mongoose.connection;
@@ -40,28 +38,33 @@ db.once('open', function callback () {
   console.log("Success");
 });
 
+//Create a schema
 var Schema = mongoose.Schema({
 	username : String,
 	FullName : String
 });
 
+console.log("Schema Created");
+//Create a model using the schema
 var userData = mongoose.model('User', Schema);
 
-var person_data = {
-    username: "",
-    FullName: ""
+//Function which accepts the data and saves it to the DB
+var saveToDb = function(person_data){
+	var person = new userData(person_data);
+	person.save();
+	console.log("Successfully stored data in db!!");
+	console.log(person_data);	
 };
 
-app.post('/', function(req, res){
+//Recieve data from page using POST method
+app.post(formPage, function(req, res){
 	console.log(req.body.username+ " "+req.body.FullName);
-	var person_data = {
-    username: req.body.username,
-    FullName: req.body.FullName
-};
-	res.send("Thank You");
+	person_data = {
+	    username: req.body.username,
+	    FullName: req.body.FullName
+	};
+	res.send("Your Data is recorded... Thank You!!\n"+"username: "+req.body.username + "\nFullName: "+ req.body.FullName);
+	saveToDb(person_data);
 	
 });
 
-var person = new userData(person_data);
-
-person.save();
